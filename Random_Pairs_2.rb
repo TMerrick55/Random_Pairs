05e5493a8673ba40b9346f636146aeabe543bd11
TMerrick55 <tmerrick44@gmail.com>
2017-01-24 18:18:06 -0500

    Random Pairs Homework Redo Ver. 2.0

--- Random_Pairs_2.rb
+++ Random_Pairs_2.rb
@@ -1,4 +1,12 @@
 def create_pairs(names)
-	array = []
-	array << names
+	names_array = names
+	shuffled = names_array.shuffle
+	sliced = shuffled.each_slice(2).to_a
+	if shuffled.length % 2 == 1
+		sliced[-2] << sliced[-1][0]
+		sliced.delete_at(-1)
+		sliced
+	else
+		sliced
+	end
 end
--- Test_Random_Pairs_2.rb
+++ Test_Random_Pairs_2.rb
@@ -12,4 +12,24 @@ class TestPairsArray < Minitest::Test
 		pairs = create_pairs(['Blake','Yang', 'Weiss', 'Ruby'])
 		assert_equal(2, pairs.count)
 	end
+
+	def test_6_names_is_3_pairs()
+		pairs = create_pairs(['Blake', 'Yang', 'Weiss', 'Ruby', 'Ozpin', 'Glynda'])
+		assert_equal(3, pairs.count)
+	end
+
+	def test_8_names_is_4_pairs()
+		pairs = create_pairs(['Blake', 'Yang', 'Weiss', 'Ruby', 'Ozpin', 'Glynda', 'Cinder', 'Emerald'])
+		assert_equal(4, pairs.count)
+	end
+
+	def test_10_names_is_5_pairs()
+		pairs = create_pairs(['Blake', 'Yang', 'Weiss', 'Ruby', 'Ozpin', 'Glynda', 'Cinder', 'Emerald', 'Amber', 'Pyrrha'])
+		assert_equal(5, pairs.count)
+	end
+
+	def test_12_names_is_6_pairs()
+		pairs = create_pairs(['Blake', 'Yang', 'Weiss', 'Ruby', 'Ozpin', 'Glynda', 'Cinder', 'Emerald', 'Amber', 'Pyrrha', 'Neo', 'Roman'])
+		assert_equal(6, pairs.count)
+	end
 end
